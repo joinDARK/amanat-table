a049626d50b296d09a72251fd6d82c26d8fb7075
Miraska <miraska.007@gmaile.com>
2025-01-27 21:13:15 +0300

    V0.0.4

--- apps/nest-server/src/main.ts
+++ apps/nest-server/src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
+
 async function bootstrap() {
+  const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+  console.log(`\nServer has started on: \x1b[34mhttp://localhost:${PORT}\x1b[0m`);
 }
+
 bootstrap();
