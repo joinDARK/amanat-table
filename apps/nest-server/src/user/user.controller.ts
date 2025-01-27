36cf871e82a5de94c478685bdbf2acc0b2acfc69
joinDARK <134739429+joinDARK@users.noreply.github.com>
2025-01-27 21:53:45 +0300

    fix:Исправлена схема валидации

--- apps/nest-server/src/user/user.controller.ts
+++ apps/nest-server/src/user/user.controller.ts
@@ -4,11 +4,12 @@ import z from 'zod';
 import { ZodValidationPipe } from 'src/zod-validation.pipe';
 
 const createUserSchema = z.object({
-  login: z.string({
-    invalid_type_error: 'Логин должен быть строкой',
-    required_error: 'Логин обязателен',
-  })
-  .min(3, 'Имя пользователя должно содержать не менее 3 символов'),
+  login: z
+    .string({
+      invalid_type_error: 'Логин должен быть строкой',
+      required_error: 'Логин обязателен',
+    })
+    .min(3, 'Имя пользователя должно содержать не менее 3 символов'),
   password: z
     .string({
       invalid_type_error: 'Пароль должен быть строкой',
@@ -28,16 +29,17 @@ const updateUserSchema = z.object({
   }),
   login: z
     .string({
-      invalid_type_error: 'Логин должен быть строкой',
-      required_error: 'Логин обязателен',
+      invalid_type_error: 'Логин должен быть строкой'
     })
-    .min(3, 'Имя пользователя должно содержать не менее 3 символов'),
+    .min(3, 'Имя пользователя должно содержать не менее 3 символов')
+    .optional(),
   password: z
     .string({
       invalid_type_error: 'Пароль должен быть строкой',
       required_error: 'Пароль обязателен',
     })
-    .min(6, 'Пароль должен содержать не менее 6 символов'),
+    .min(6, 'Пароль должен содержать не менее 6 символов')
+    .optional(),
   roleId: z.number({
     invalid_type_error: 'ID роли должен быть числом',
     required_error: 'ID роли обязателен',
